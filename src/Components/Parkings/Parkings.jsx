ed0ef0f58098ad8f6ec4ffd145f0e7f8f5a4f144
neria <neria.levi444@gmail.com>
2023-01-25 17:53:21 +0200

    FILTER

--- src/Components/Parkings/Parkings.jsx
+++ src/Components/Parkings/Parkings.jsx
@@ -4,38 +4,32 @@ import { MyContext } from "../../App";
 
 export default function Parkings() {
   const { data } = useContext(MyContext);
+  const [inputData, setInput] = useState("");
+
+  const countryInput = function (input) {
+    setInput(input.charAt(0).toUpperCase() + input.slice(1));
+    console.log(inputData);
+  };
+
 
   return (
     <>
     <div className="d-flex justify-content-end m-3">
       <form class="col-2 d-flex form-inline my-2 my-lg-0">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e) => countryInput(e.target.value)}/>
       </form>
     </div>
     
     <div className="row justify-content-center">
       <div className="row justify-content-around container ">
-        {data.map((item, i) => (
-          <Link
-            to={item.id}
-            key={i}
-            className="card border m-1"
-            style={{ width: "350px", height: "450px" }}
-          >
+        {data.filter((country) => country.name.startsWith(inputData)).map((item, i) => (
+          <Link to={item.id} key={i} className="card border m-1" style={{ width: "350px", height: "450px" }}>
             <h4 className="d-flex justify-content-center">{item.name}</h4>
-            <div
-              className="div-imges d-flex justify-content-center"
-              style={{ height: "65%" }}
-            >
-              <img
-                className="img-card border"
-                src={item.img}
-                alt={item.name}
-                style={{ height: "85%", width: "100%" }}
-              />
+            <div className="div-imges d-flex justify-content-center" style={{ height: "65%" }}>
+              <img className="img-card border" src={item.img} alt={item.name} style={{ height: "85%", width: "100%" }}/>
             </div>
-            <h2 className="d-flex justify-content-center">{item.price}💲</h2>
+            <h2 className="d-flex justify-content-center">{item.price}₪</h2>
             <div className=" d-flex justify-content-center">
               <button type="button" class="btn btn-success">
                 Detail Parking
