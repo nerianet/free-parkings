9f0df19923bf5e2e940069bdcaaeddb375106c93
neria <neria.levi444@gmail.com>
2023-01-29 14:11:27 +0200

    S

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -27,17 +27,17 @@ export default function Header() {
       </div>
       
 
-    {/* <div class="btn-group" role="group">
-      <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <div class="btn-group" role="group">
+      <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
         Dropdown
       </button>
-      <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-        <a class="dropdown-item" href="#">Dropdown link</a>
-        <a class="dropdown-item" href="#">Dropdown link</a>
-      </div>
-    </div> */}
+      <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+        <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+        <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+      </ul>
+    </div>
 
-    </ul>
+  </ul>
     </>
   )
 }
