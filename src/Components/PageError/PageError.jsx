1fc96da722c7e5328d488020f6020fb4fdb0dbf7
neria <neria.levi444@gmail.com>
2023-01-30 10:11:14 +0200

    m

--- src/Components/PageError/PageError.jsx
+++ src/Components/PageError/PageError.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function PageError() {
   return (
     <>
+    <div className="container"></div>
       <div className="row justify-content-center mt-5">
         <h1>page note found 404</h1>
       </div>
