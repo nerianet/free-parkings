7b00a7d95f9ad779c5cf10184f690c86a5565969
moshe <moshe6073163@gmail.com>
2023-01-30 18:19:17 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ export default function App() {
   return (
     <div className="bg_site vh-100 ">
       <div className="bg_site">
-        <Location/>
+        {/* <Location/> */}
         <MyContext.Provider value={AllData}>
           <Header />
           <Routes>
