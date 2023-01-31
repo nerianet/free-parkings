6aa16e4417d492c792d7f9cd5c05a50cbc670f70
neria <neria.levi444@gmail.com>
2023-01-31 10:47:49 +0200

    m

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,13 +15,15 @@ export default function Footer() {
   const [input, setInput] = useState("");
   fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=${API_KEY}`, requestOptions)
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then(result => setData(result))
   .catch(error => console.log('error', error));
 
   return (
     <div className='container align-text-bottom a w-50'>
       <input onChange={e=>setInput(e.target.value)} />
-
+      <div>{data.feutures.map((e)=>{
+        
+      })}</div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellendus non porro quidem laboriosam soluta dolorum delectus enim cum! Enim, repudiandae optio nesciunt illum tempora ea id eum laboriosam quibusdam blanditiis quia! Natus mollitia at autem animi, commodi expedita. Ad velit soluta magnam doloremque dolore voluptas architecto consequatur itaque molestias optio repellat nisi inventore, laborum eius asperiores id nam alias.
     </div>
   )
