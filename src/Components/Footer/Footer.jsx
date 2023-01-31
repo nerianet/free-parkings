0fd618140f23afc5354e581fcf3ccc1a5d430871
moshe <moshe6073163@gmail.com>
2023-01-31 21:26:04 +0200

    m

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,22 +8,22 @@ var requestOptions = { method: 'GET' };
 export default function Footer() {
   const [data, setData] = useState([]);
   const [input, setInput] = useState("");
-  useEffect(()=>{
-    fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=${API_KEY}`, requestOptions)
-    .then(response => response.json())
-    .then(result => setData(result.features != undefined ? result.features : []))
-    .catch(error => console.log('error', error));
-  },[input])
+  // useEffect(()=>{
+  //   fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=${API_KEY}`, requestOptions)
+  //   .then(response => response.json())
+  //   .then(result => setData(result.features != undefined ? result.features : []))
+  //   .catch(error => console.log('error', error));
+  // },[input])
   
   return (
     <div className='container align-text-bottom a w-50'>
       <input onChange={e=>setInput(e.target.value)} />
       <div className='container'>
-      {
+      {/* {
         data.map((e)=>(
           <div className='rounded text-light border'> {e.properties.formatted }</div>
         ))
-      }
+      } */}
       </div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellendus non porro quidem laboriosam soluta dolorum delectus enim cum! Enim, repudiandae optio nesciunt illum tempora ea id eum laboriosam quibusdam blanditiis quia! Natus mollitia at autem animi, commodi expedita. Ad velit soluta magnam doloremque dolore voluptas architecto consequatur itaque molestias optio repellat nisi inventore, laborum eius asperiores id nam alias.
     </div>
