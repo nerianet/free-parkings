869b188bfe8255360b8cf0e5cc9aee925508a760
moshe <moshe6073163@gmail.com>
2023-01-31 10:43:05 +0200

    m

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 
-const api_key = "0d1d8fa39ae14774b80d34e9c2fe719e";
-
+const API_KEY = "0d1d8fa39ae14774b80d34e9c2fe719e";
 var requestOptions = {
   method: 'GET',
 };
 
-fetch(`https://api.geoapify.com/v1/geocode/autocomplete?${ap}=Mosco&apiKey=${api_key}`, requestOptions)
+
+
+
+
+export default function Footer() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${input}&apiKey=${API_KEY}`, requestOptions)
   .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 
-export default function Footer() {
   return (
     <div className='container align-text-bottom a w-50'>
-      <input />
+      <input onChange={e=>setInput(e.target.value)} />
 
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum repellendus non porro quidem laboriosam soluta dolorum delectus enim cum! Enim, repudiandae optio nesciunt illum tempora ea id eum laboriosam quibusdam blanditiis quia! Natus mollitia at autem animi, commodi expedita. Ad velit soluta magnam doloremque dolore voluptas architecto consequatur itaque molestias optio repellat nisi inventore, laborum eius asperiores id nam alias.
     </div>
