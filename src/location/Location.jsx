4eaa3b75e019763de829fb814b55051409b9bb07
moshe <moshe6073163@gmail.com>
2023-02-12 15:10:53 +0200

    m

--- src/location/Location.jsx
+++ src/location/Location.jsx
@@ -7,10 +7,24 @@ export default function Location() {
   const {setCordUser, cordUser} = useContext(MyContext);
 
   const v = useGeolocated();
-
+  let L = "https://api.geoapify.com/v1/icon/?type=material&color=red&icon=cloud&iconType=awesome&apiKey=0d1d8fa39ae14774b80d34e9c2fe719e";
+  const m = document.getElementById('momo');
+  function KokaKola(){
+    const markerIcon = L.icon({
+    iconUrl: `https://api.geoapify.com/v1/icon?size=xx-large&type=awesome&color=%233e9cfe&icon=paw&apiKey=0d1d8fa39ae14774b80d34e9c2fe719e`,
+    iconSize: [31, 46], // size of the icon
+    iconAnchor: [15.5, 42], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -45] // point from which the popup should open relative to the iconAnchor
+  });
+  const zooMarker = L.marker([cordUser.longitude, cordUser.latitude], {
+    icon: markerIcon
+  }).addTo(m);
+}
+  
   useEffect(()=>{
     if(v.coords != undefined){
       setCordUser({latitude : v.coords.latitude, longitude: v.coords.longitude});
+      KokaKola();
     }
   }, [v.coords])
 
@@ -18,11 +32,11 @@ export default function Location() {
  
   return (
     <div>
-        {cordUser != undefined ? <iframe src={`https://maps.geoapify.com/v1/staticmap?style=positron&width=800&height=600&center=lonlat:${cordUser.longitude},${cordUser.latitude}&zoom=11.7625&marker=lonlat:${cordUser.longitude},${cordUser.latitude};type:awesome;color:%23aa3f79;size:x-large;icon:parking&apiKey=0d1d8fa39ae14774b80d34e9c2fe719e`}
-        width="900px" style={{height:"500px", border:'0' }} 
-        
-        loading="lazy">
+        {cordUser != undefined ? <iframe id='momo' src={`https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=600&height=400&center=lonlat:${cordUser.longitude},${cordUser.latitude}&zoom=14&apiKey=0d1d8fa39ae14774b80d34e9c2fe719e`}
+        width="900px" style={{height:"500px", border:'0' }}
+        loading="lazy" >
         </iframe> : ""}
+        {/* <img src='icon?type=material&color=red&size=small&icon=cloud&iconType=awesome&noShadow'/> */}
   </div>
   )
 }
