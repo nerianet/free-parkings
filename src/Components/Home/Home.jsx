46d678f83dca69c21c6adee2509a6329b3766220
neria <neria.levi444@gmail.com>
2023-02-27 23:55:57 +0200

    j

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   let v = document.getElementById('search');
   function startSearch(){
-    setInput(v.value);
+    setInput(v.value.charAt(0).toUpperCase() + v.value.slice(1));
   }
   
   function addToIn(e){
@@ -27,19 +27,19 @@ export default function Home() {
   return (
   <>
   <div className="container-fluid row">
-    <div className="col-12 ">
-    <h1 className="d-flex justify-content-center display-2">Search Parkings</h1>
+    <div className="col-12">
+      <h1 className="d-flex justify-content-center display-2">Search Parkings</h1>
       <p className="d-flex justify-content-center pb-5 fs-1">Here you can easily find parking</p>
       <div className="d-flex justify-content-center display-6">Choose City ...</div>
       <div className="d-flex justify-content-center col-12">
         <div className="col-sm-6 col-12">
-          <div class="input-group mb-3 d-flex justify-content-center w-sm-75 rounded" style={{border: "4px solid rgb(111, 184, 184)", shadow: "2px 2px 12px 2px rgb(12, 11, 11)" }}>
+          <div class="input-group d-flex justify-content-center w-sm-75 rounded" style={{border: "4px solid rgb(111, 184, 184)", shadow: "2px 2px 12px 2px rgb(12, 11, 11)" }}>
             <input id="search" type="text" className="form-control" placeholder="Please Enter City" onChange={e=>getData(e.target.value) } />
             <div class="input-group-append">
               <Link to={'/Parkings'}><button class="btn btn-outline-secondary" type="button" onClick={startSearch}>Search</button></Link>
             </div>
           </div>
-          <div className="posotion-relative" style={{width: '92%'}}>{inputData.map((e, i)=>(
+          <div className="">{inputData.map((e, i)=>(
             <div className="border bg-light">{i < 1 ? <div onClick={e=>addToIn(e.target.innerHTML)} className="btn text-dark" >{e.properties.city}</div> 
               : 
               inputData[i].properties.city == inputData[i-1].properties.city ? ""
@@ -51,7 +51,7 @@ export default function Home() {
       </div>
 
     </div>
-    <div className="h-50 d-flex justify-content-center flex-wrap mb-3">
+    <div className="h-50 d-flex justify-content-center flex-wrap mb-3 ">
       <div className="col-sm-5 col-10 mx-sm-3 d-flex justify-content-center mb-sm-0 mb-2 rounded">
         <img src="https://i.imagesup.co/images2/6d4130c2afe7821e921360c5d3c789ed38a6ab64.png" width={'100%'}/>
       </div>
